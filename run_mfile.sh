55ecdcbb2a36de862ffb096028eb5e9f16200ddb
Torben Ott <tott@cshl.edu>
2018-05-28 20:44:27 -0400

    run_mfile script MATLAB compatible (suppress first output lines)

--- run_mfile.sh
+++ run_mfile.sh
@@ -2,15 +2,17 @@
 
 if ( command -v matlab &> /dev/null )
 then
-  cmd='matlab -nosplash -nojvm -r'
+  matlab -nosplash -nojvm -r "try, cd(""'"$(dirname $0)"'""), $1, exit(0), catch err, disp(getReport(err,'extended')), exit(-1), end" | tail -n +13 
 else
   if ( command -v octave &> /dev/null )
   then
-    cmd='octave --no-gui --eval'
+    octave --no-gui --eval "try, cd(""'"$(dirname $0)"'""), $1, exit(0), catch err, disp(getReport(err,'extended')), exit(-1), end"
   else
     printf "\nCould not find Matlab nor Octave.\n\tMake sure at least one is installed and callable.\n\tError at $0\n\tCalled with arguments: $@\n\n"
     exit 1
   fi
 fi
-$cmd "cd(""'"$(dirname $0)"'), $1"
-exit 0
+
+
+
+

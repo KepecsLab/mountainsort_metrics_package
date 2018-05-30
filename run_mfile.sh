9883e3954c0b9a88f9f49984843e950b9558a7e0
Thiago@Oxossi <tsgouvea@cshl.edu>
2018-05-30 18:01:33 -0400

    Bugfix - Octave command reverted to what was before Matlab bugfix

--- run_mfile.sh
+++ run_mfile.sh
@@ -6,7 +6,7 @@ then
 else
   if ( command -v octave &> /dev/null )
   then
-    octave --no-gui --eval "try, cd(""'"$(dirname $0)"'""), $1, exit(0), catch err, disp(getReport(err,'extended')), exit(-1), end"
+    octave --no-gui --eval "cd(""'"$(dirname $0)"'""), $1, exit(0)"
   else
     printf "\nCould not find Matlab nor Octave.\n\tMake sure at least one is installed and callable.\n\tError at $0\n\tCalled with arguments: $@\n\n"
     exit 1
